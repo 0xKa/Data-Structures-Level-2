fa35b1217d59358b98fb3fcd1943658010d7264d
Reda Hilal <reda0x04@gmail.com>
2025-03-23 05:57:36 +0400

    lists

--- 03-Lists/Program.cs
+++ 03-Lists/Program.cs
@@ -15,6 +15,19 @@ using System.Linq;
 
 internal class Program
 {
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(int ID, string name, int age)
+        {
+            this.ID = ID;
+            this.Name = name;
+            this.Age = age;
+        }
+    }
 
     public static List<string> stringList = new List<string>
     {
@@ -28,9 +41,19 @@ internal class Program
 
     public static List<int> numbersList = new List<int>
     {
-        666, 21 ,34 ,325, 5132, 63, 91, -8, 329, -940, 1221, 312, 832, -54, 2, 16, 37, -8, -100, 20
+        666, 21 ,34 ,325, 5132, 63, 91, -8, 329, -940, 1221, 312, 832, -54, 2, 16, 37, -8, 720, -100
     };
 
+    public static List<Person> PeopleList = new List<Person>
+    {
+        new Person(1, "Mohammed", 20),
+        new Person(2, "Ahmed", 23),
+        new Person(3, "Reda", 20),
+        new Person(4, "Salim", 18),
+        new Person(5, "Ali", 26),
+        new Person(6, "Omar", 25),
+        new Person(7, "Saif", 29)
+    };
 
     public static void PrintList<T>(List<T> list, string title = null)
     {
@@ -107,6 +130,73 @@ internal class Program
 
 
     }
+    public static void CheckingList()
+    {
+        Console.WriteLine("List Contains [12]? : " + numbersList.Contains(12));
+
+        Console.WriteLine("\nCondition Exists [n > 20]? :" + numbersList.Exists(n => n > 20)); 
+        Console.WriteLine("Condition Exists (using .Any())[n > 20]? :" + numbersList.Any(n => n > 20)); //same as Exists, but for more collections
+
+        Console.WriteLine("\nFind First Element that [n >= 20]: " + numbersList.Find(n => n >= 20)); 
+        Console.WriteLine("Find Last Element that [n >= 20]: " + numbersList.FindLast(n => n >= 20)); 
+        Console.WriteLine("Find First Index of Element that [n >= 20]:" + numbersList.FindIndex(n => n >= 20));
+        Console.WriteLine("Find Last Index of Element that [n >= 20]:" + numbersList.FindLastIndex(n => n >= 20));
+        
+
+
+        Console.WriteLine("\nFind All Elements that [n >= 500]:");
+        PrintList(numbersList.FindAll(n => n >= 500));
+
+        /* .Exists() vs .Any()
+                -Exists is specific to List<T> and is available directly on instances of List<T>.
+                -Any is a LINQ extension method available for any collection implementing IEnumerable<T>.
+                -Both methods serve similar purposes, but Exists is more specialized for lists, while Any is more versatile and can be used with any enumerable collection.
+                -Exists is more efficient than Any for lists because it directly operates on the list without the overhead of LINQ. However, for collections other than lists, Any is often the only option.
+         */
+
+    }
+    public static void ListWithCustomObject()
+    {
+        Console.WriteLine("People List:");
+        foreach (Person person in PeopleList)
+        {
+            Console.WriteLine(person.ID + ", " + person.Name + ", " + person.Age);
+        }
+
+        Console.WriteLine("\nFinding Person Name [Ali]: " + PeopleList.Exists(p => p.Name == "Ali"));
+
+        Console.WriteLine("\nFinding all people that [Age >= 25]: ");
+        List<Person> peopleOver25 = PeopleList.FindAll(p => p.Age >= 25);
+        foreach (Person person in peopleOver25)
+        {
+            Console.WriteLine(person.ID + ", " + person.Name + ", " + person.Age);
+        }
+
+        Console.WriteLine("\nRemoving all people that [Age >= 25]: ");
+        Console.WriteLine(PeopleList.RemoveAll(p => p.Age >= 25) + " People Removed.");
+        foreach (Person person in PeopleList)
+        {
+            Console.WriteLine(person.ID + ", " + person.Name + ", " + person.Age);
+        }
+
+
+    }
+    public static void ConvertListToArray()
+    {
+        //convert list to array
+        int[] arr = numbersList.ToArray();
+
+        Console.WriteLine(string.Join(", ", arr));
+
+        //convert array to list
+        List<int> NumbersList = new List<int>(arr);
+        PrintList(NumbersList);
+
+        //another way
+        NumbersList = arr.ToList();
+        PrintList(NumbersList);
+
+    }
 
     static void Main(string[] args)
     {
@@ -120,7 +210,14 @@ internal class Program
 
         //FilteringDatawithLINQ();
 
-        SortingAList();
+        //SortingAList();
+
+        //CheckingList();
+
+        //ListWithCustomObject();
+
+        //ConvertListToArray();
+
 
         //PrintList<string>(stringList);
         //PrintList<int>(numbersList);
