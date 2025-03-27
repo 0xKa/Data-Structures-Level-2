2363a7d41a95f23efabe3779d90ac61b20a8670c
Reda Hilal <reda0x04@gmail.com>
2025-03-28 02:38:41 +0400

    Queue-Stack

---
+++ 12-QueueStack/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Program
+{
+    public static void Queue_Example()
+    {
+        Queue<string> queue = new Queue<string>();
+
+        // Enqueue items into the queue
+        queue.Enqueue("C#");
+        queue.Enqueue("Java");
+        queue.Enqueue("Python");
+        Console.WriteLine("Items in Queue: " + string.Join(", ", queue));
+
+        // Peek the front item
+        Console.WriteLine("Front Item: " + queue.Peek());
+
+        // Dequeue items from the queue
+        Console.WriteLine("Dequeued Item: " + queue.Dequeue());
+        Console.WriteLine("Queue After Dequeue: " + string.Join(", ", queue));
+
+        // Check if the queue contains an item
+        Console.WriteLine("Contains 'Python': " + queue.Contains("Python"));
+
+        // Get count of items
+        Console.WriteLine("Number of Items: " + queue.Count);
+
+        // Clear the queue
+        queue.Clear();
+        Console.WriteLine("Queue Cleared. Count: " + queue.Count);
+    }
+    public static void Stack_Example()
+    {
+        Stack<string> stack = new Stack<string>();
+
+        // Push items onto the stack
+        stack.Push("C#");
+        stack.Push("Java");
+        stack.Push("Python");
+        Console.WriteLine("Items in Stack: " + string.Join(", ", stack));
+
+        // Peek the top item
+        Console.WriteLine("Top Item: " + stack.Peek());
+
+        // Pop items from the stack
+        Console.WriteLine("Popped Item: " + stack.Pop());
+        Console.WriteLine("Stack After Pop: " + string.Join(", ", stack));
+
+        // Check if the stack contains an item
+        Console.WriteLine("Contains 'C#': " + stack.Contains("C#"));
+
+        // Get count of items
+        Console.WriteLine("Number of Items: " + stack.Count);
+
+        // Clear the stack
+        stack.Clear();
+        Console.WriteLine("Stack Cleared. Count: " + stack.Count);
+    }
+    
+
+    static void Main(string[] args)
+    {
+        Queue_Example();
+
+        Console.WriteLine("\n\n\n");
+        
+        Stack_Example();
+
+        Console.WriteLine("\n\n\n");
+    }
+
+}
---
+++ 12-QueueStack/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("12-QueueStack")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("12-QueueStack")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e94ad0f3-435a-45c4-afb9-2e3b92b90a87")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
